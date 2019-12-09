9be7d8a1282b943dfab593c781ea03f3c9213ba9
kangkang59812 <596286458@qq.com>
2019-12-09 15:03:56 +0800

    just for debug the process, 运行超级慢

--- myroialign.py
+++ myroialign.py
@@ -1,6 +1,7 @@
 import torch
 import numpy
 from torch import nn
+from tqdm import tqdm
 
 
 class roiAlign(nn.Module):
@@ -23,11 +24,10 @@ class roiAlign(nn.Module):
         roi_cols = 5
         # n_rois=512, 4个128
         n_rois = int(nthreads / channels / pooled_width / pooled_height)
-
-        for n in range(n_rois):
-            # index_n = n * channels * pooled_width * pooled_height
-            # offset_bottom_rois = bottom_rois + n * roi_cols
-
+        top_data = torch.empty(size=(512, 1024, 7, 7))
+        for n in tqdm(range(n_rois)):
+            index_n = n * channels * pooled_width * pooled_height
+            roi_batch_ind = int(bottom_rois[n][0].item())
             roi_start_w = bottom_rois[n][1] * self.spatial_scale
             roi_start_h = bottom_rois[n][2] * self.spatial_scale
             roi_end_w = bottom_rois[n][3] * self.spatial_scale
@@ -44,13 +44,51 @@ class roiAlign(nn.Module):
 
             count = roi_bin_grid_h * roi_bin_grid_w
             bilinear = bilinear_interpolate(
-                roi_bin_grid_h * roi_bin_grid_w * pooled_width * pooled_height)
+                num=roi_bin_grid_h * roi_bin_grid_w * pooled_width * pooled_height,
+                height=self.height,
+                width=self.width,
+                iy_upper=roi_bin_grid_h,
+                ix_upper=roi_bin_grid_w,
+                roi_start_h=roi_start_h,
+                roi_start_w=roi_start_w,
+                bin_size_h=bin_size_h,
+                bin_size_w=bin_size_w,
+                roi_bin_grid_h=roi_bin_grid_h,
+                roi_bin_grid_w=roi_bin_grid_w)
+
+            pre_calc = bilinear()
+
+            for c in range(channels):
+                # _c = index_n + c * pooled_width * pooled_height
+                offset_bottom_data = bottom_data[roi_batch_ind][c].view(-1)
+                pre_calc_index = 0
+
+                for ph in range(pooled_height):
+                    for pw in range(pooled_width):
+                        # index = index_n_c + ph * pooled_width + pw
+                        output_val = 0.
+
+                        for iy in range(roi_bin_grid_h):
+                            for ix in range(roi_bin_grid_w):
+                                pc = pre_calc[pre_calc_index]
+                                output_val += pc[4] * offset_bottom_data[int(pc[0])] + \
+                                    pc[5] * offset_bottom_data[int(pc[1])] + \
+                                    pc[6] * offset_bottom_data[int(pc[2])] + \
+                                    pc[7] * offset_bottom_data[int(pc[3])]
+
+                                pre_calc_index += 1
+
+                        output_val /= count
+                        top_data[n][c][ph][pw] = output_val
+            print('')
+        return top_data
 
 
 class bilinear_interpolate(nn.Module):
     '''
     提前计算双线性插值位置
     '''
+
     def __init__(self, num=None,
                  height=None,
                  width=None,
@@ -79,8 +117,62 @@ class bilinear_interpolate(nn.Module):
         self.roi_bin_grid_h = roi_bin_grid_h  # 3
         self.roi_bin_grid_w = roi_bin_grid_w  # 4
 
-    def forward(self,):
+    def forward(self):
         pre_calc = torch.empty(self.num, 8)
+        pre_calc_index = 0
+
+        for ph in range(self.pooled_height):
+            for pw in range(self.pooled_width):
+                for iy in range(self.iy_upper):
+                    yy = torch.tensor(self.roi_start_h + ph * self.bin_size_h +
+                                      (iy + 0.5) * self.bin_size_h / self.roi_bin_grid_h)
+                    for ix in range(self.ix_upper):
+                        xx = torch.tensor(self.roi_start_w + pw * self.bin_size_w +
+                                          (ix + 0.5) * self.bin_size_w / self.roi_bin_grid_w)
+
+                        x = xx.item()
+                        y = yy.item()
+
+                        if (y < -1.0 or y > self.height or x < -1.0 or x > self.width):
+                            pre_calc[pre_calc_index, :] = 0
+                            continue
+                        if y <= 0:
+                            y = 0
+                        if x <= 0:
+                            x = 0
+
+                        y_low = int(y)
+                        x_low = int(x)
+                        y_high = None
+                        x_high = None
+
+                        if y_low >= height - 1:
+                            y_high = height - 1
+                            y_low = height - 1
+                            y = y_low
+                        else:
+                            y_high = y_low + 1
+
+                        if x_low >= width - 1:
+                            x_high = width - 1
+                            x_low = width - 1
+                            x = x_low
+                        else:
+                            x_high = x_low + 1
+
+                        ly = y - y_low
+                        lx = x - x_low
+                        hy = 1. - ly
+                        hx = 1. - lx
+                        w1 = hy * hx
+                        w2 = hy * lx
+                        w3 = ly * hx
+                        w4 = ly * lx
+                        pre_calc[pre_calc_index] = torch.tensor(
+                            [y_low * width + x_low, y_low * width + x_high, y_high * width + x_low, y_high * width + x_high, w1, w2, w3, w4])
+                        pre_calc_index += 1
+
+        return pre_calc
 
 
 bottom_data = torch.load('/home/lkk/code/copyfaster/input.pt')
